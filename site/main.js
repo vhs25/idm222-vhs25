474f8edfdb2d9a9a78ef84186e5df91421947048
Victoria Stauffer <vstauffer21@gmail.com>
2020-03-09 20:58:03 -0400

    work on artwork modal
    img access not quite working

---
+++ site/main.js
@@ -0,0 +1,26 @@
+const btns = document.querySelectorAll('.btn');
+const popup = document.getElementById('pop-up');
+const picture = document.getElementById('picture');
+
+
+
+const handleButtonClick = whichOne =>{
+    console.log(whichOne);
+    console.log(whichOne.target);
+    console.log(whichOne.target.id);
+    //picture(whichOne.target.id);
+    const num = whichOne.target.id;
+    picture.src = 'med/${num}_med.png';
+
+    popup.hidden = false;
+}
+
+btns.forEach(btn => {
+    btn.addEventListener('click', event =>{
+        handleButtonClick(event);
+    });
+});
+
+popup.addEventListener('click', event =>{
+    popup.hidden = true;
+})
