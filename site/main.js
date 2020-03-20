49092e73c23a78755376b8e435e6b214c4dc3b89
Victoria Stauffer <vstauffer21@gmail.com>
2020-03-19 22:47:13 -0400

    nearly there

--- site/main.js
+++ site/main.js
@@ -1,15 +1,24 @@
 const btns = document.querySelectorAll('.btn');
 const popup = document.getElementById('pop-up');
 const picture = document.getElementById('picture');
-
+var window_width = window.screen.availWidth;
+console.log(' width: ', window_width);
 
 const handleButtonClick = whichOne =>{
     console.log(whichOne);
     console.log(whichOne.target);
     console.log(whichOne.target.id);
     const num = whichOne.target.id;
-    picture.src = `med/${num}_med.png`;
 
+    if(window_width <= 999){
+        picture.src = `med/${num}_med.png`;
+        console.log(num);
+    }
+    else if(window_width >= 1000){
+        picture.src = `large/${num}_large.png`;
+    }
+    
+    //window.location.reload();
     popup.hidden = false;
 }
 
