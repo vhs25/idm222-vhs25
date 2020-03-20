a5c38f43f10af395361a03483c8029fc0e2cd0f2
Victoria Stauffer <vstauffer21@gmail.com>
2020-03-19 22:58:36 -0400

    Done!

--- site/main.js
+++ site/main.js
@@ -5,25 +5,26 @@ var window_width = window.screen.availWidth;
 console.log(' width: ', window_width);
 
 const handleButtonClick = whichOne =>{
+    
     console.log(whichOne);
     console.log(whichOne.target);
     console.log(whichOne.target.id);
     const num = whichOne.target.id;
 
-    if(window_width <= 999){
+    if(window_width <= 900){
         picture.src = `med/${num}_med.png`;
         console.log(num);
     }
-    else if(window_width >= 1000){
+    else if(window_width >= 900){
         picture.src = `large/${num}_large.png`;
     }
     
-    //window.location.reload();
     popup.hidden = false;
 }
 
 btns.forEach(btn => {
     btn.addEventListener('click', event =>{
+        //window.location.reload();
         handleButtonClick(event);
     });
 });
